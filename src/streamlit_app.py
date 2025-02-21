7a7dc7834ebe79919f66267a0f576c42a3363914
smeisegeier <dexterDSD@googlemail.com>
2025-02-21 21:18:22 +0100

    back to 3.12

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,28 +2,20 @@ import os
 import pandas as pd
 from pathlib import Path
 import streamlit as st
+from pandas_plots import hlp
 
 # import pygwalker as pyg
 from pygwalker.api.streamlit import StreamlitRenderer
 
 # os.environ["THEME"]="dark"
+title ="sport-adverse-events"
 
 # Adjust the width of the Streamlit page
-st.set_page_config(page_title="Use Pygwalker In Streamlit", layout="wide")
+st.set_page_config(page_title=title, layout="wide")
 
 # Add Title
-st.title("Use Pygwalker In Streamlit")
+st.title(title)
 
-# def get_cols(all_cols: list[str], stubs = list[str]):
-#     return [col for col in all_cols if any(match in col for match in stubs)]
-
-
-df_csv_condensed = pd.read_csv(
-    "https://raw.githubusercontent.com/smeisegeier/sport-adverse-events/refs/heads/main/data/out/df_csv_condensed.csv",
-    sep=";",
-    encoding="utf-8-sig",
-    index_col=None,
-)
 
 # * mark columns that should be str instead of float, fuzzy
 # col_int = get_cols(all_cols=df_csv_condensed.columns, stubs=["[01.01]"])
@@ -38,17 +30,23 @@ df_csv_condensed = pd.read_csv(
 # You should cache your pygwalker renderer, if you don't want your memory to explode
 @st.cache_resource
 def get_pyg_renderer() -> "StreamlitRenderer":
-    df_csv_condensed = pd.read_csv(
+    df_csv_condensed = (pd.read_csv(
         "https://raw.githubusercontent.com/smeisegeier/sport-adverse-events/refs/heads/main/data/out/df_csv_condensed.csv",
         sep=";",
         encoding="utf-8-sig",
         index_col=None,
     )
+    .iloc[:,1:]     # omit first column, which is just the id
+)
+
+    cols_to_str = hlp.find_cols(all_cols=df_csv_condensed.columns, stubs = ["[03.04.02]", "[07.04]", "[03.01.01]", "[01.01]"])
+    df_csv_condensed[cols_to_str] = df_csv_condensed[cols_to_str].astype(str)
 
     # If you want to use feature of saving chart config, set `spec_io_mode="rw"`
     return StreamlitRenderer(
         dataset=df_csv_condensed,
-        # spec="./gw_config.json",
+        spec="./streamlit_config.json",
+        kernel_computation=True,
         spec_io_mode="rw",
     )
 
@@ -56,14 +54,3 @@ def get_pyg_renderer() -> "StreamlitRenderer":
 renderer = get_pyg_renderer()
 
 renderer.explorer()
-
-
-# # Render PyGWalker widget
-# pyg_app = pyg.walk(
-#     df_csv_condensed,
-#     kernel_computation=True,
-# ).to_html()
-
-# # Embed the PyGWalker app in Streamlit
-# # st.components.v1.html(pyg_app)
-# st.components.v1.html(pyg_app, height=1000, scrolling=True)
