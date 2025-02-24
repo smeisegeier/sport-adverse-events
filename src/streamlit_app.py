9235293f87d0a79512012e547ea0448e25562252
smeisegeier <dexterDSD@googlemail.com>
2025-02-24 15:02:23 +0100

    fixed stubs

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import streamlit as st
 from pandas_plots import hlp
 
+# test locally with:
+# streamlit run src/streamlit_app.py
+
 # import pygwalker as pyg
 from pygwalker.api.streamlit import StreamlitRenderer
 
@@ -16,17 +19,6 @@ st.set_page_config(page_title=title, layout="wide")
 # Add Title
 st.title(title)
 
-
-# * mark columns that should be str instead of float, fuzzy
-# col_int = get_cols(all_cols=df_csv_condensed.columns, stubs=["[01.01]"])
-
-# # *
-# df_csv_condensed = (df_csv_condensed
-#         .astype({col_int[0]:"Int64"})
-#         .astype({col_int[0]:str})
-# )
-
-
 # You should cache your pygwalker renderer, if you don't want your memory to explode
 @st.cache_resource
 def get_pyg_renderer() -> "StreamlitRenderer":
@@ -37,9 +29,21 @@ def get_pyg_renderer() -> "StreamlitRenderer":
         index_col=None,
     )
     .iloc[:,1:]     # omit first column, which is just the id
-)
+    )
+
+    # * mark columns that should be str instead of float, fuzzy
+    col_int = hlp.get_cols(all_cols=df_csv_condensed.columns, stubs=["[01.01]"])
 
     cols_to_str = hlp.find_cols(all_cols=df_csv_condensed.columns, stubs = ["[03.04.02]", "[07.04]", "[03.01.01]", "[01.01]"])
+
+    # * change the type of the columns
+    # df_csv_condensed = (df_csv_condensed
+    #         .astype({col_int[0]:"Int64"})
+    #         .astype({col_int[0]:str})
+    # )
+    df_csv_condensed[col_int] = df_csv_condensed[col_int].astype("Int64")
+    df_csv_condensed[col_int] = df_csv_condensed[col_int].astype(str)
+    
     df_csv_condensed[cols_to_str] = df_csv_condensed[cols_to_str].astype(str)
 
     # If you want to use feature of saving chart config, set `spec_io_mode="rw"`
